78a25957d7ab35b1d17a89f4d8dd246152820823
xxxtwitterxxx06 <123307758+xxxtwitterxxx06@users.noreply.github.com>
2023-01-23 11:43:59 +0800

    Add files via upload
    Initial prelim changes

---
+++ prelims/node-example.js
@@ -0,0 +1,12 @@
+//Name: Miguel P. Lee
+//Modeule: 3
+//week 2 and 3: Basics of NodeJS
+
+//first program
+console.log("Welcome to WCSERVER");
+
+//creating my first function
+function Display(x) {
+    console.log(x);
+}
+ Display(200);

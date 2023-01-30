0a472d5eb00427a3a16ec766f2f3876d13d86382
xxxtwitterxxx06 <123307758+xxxtwitterxxx06@users.noreply.github.com>
2023-01-30 13:10:23 +0800

    1 file modified

--- prelims/Lab-Activity-3/Lab-Activity-3-NodeJs-Server.js
+++ prelims/Lab-Activity-3/Lab-Activity-3-NodeJs-Server.js
@@ -5,21 +5,21 @@ var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     res.write(
-      '<html><h1>Welcome to my NodeJs Application!</h1><p><body>Welcome Miguel Lee. This is an Activity about basics of Node.js</p></body></html>'
+      '<html><h1 style="color:blue;">Welcome to my NodeJs Application!</h1><p><body>Welcome Miguel Lee. This is an Activity about basics of Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     res.write(
-      '<html><h1>This is about page</h1><p><body>Hello Miguel Lee. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
+      '<html><h1 style="color:blue;">This is about page</h1><p><body>Hello Miguel Lee. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
     );
     res.end();
   } else if (req.url == '/contact') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     res.write(
-      '<html><h1>This is the Contact Page</h1><p><body>Miguel Lee, if you want additional deatils about this activity go to this site: https://www.tutorialsteacher.com/nodejs/nodejs-tutorials</p></body></html>'
+      '<html><h1 style="color:blue;">This is the Contact Page</h1><p><body>Miguel Lee, if you want additional deatils about this activity go to this site: https://www.tutorialsteacher.com/nodejs/nodejs-tutorials</p></body></html>'
     );
     res.end();
   } else if (req.url == '/gallery') {

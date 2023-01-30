1d76e7f2a35e2250aa1a08140d58dc0bdf3b4611
xxxtwitterxxx06 <123307758+xxxtwitterxxx06@users.noreply.github.com>
2023-01-30 22:50:57 +0800

    1 file modified, 1 added

--- prelims/Prelim-Hands-On-Quiz/app2.js
+++ prelims/Prelim-Hands-On-Quiz/app2.js
@@ -9,15 +9,18 @@ var sss = 1200;
 var pagibig = 300;
 var philhealth = 400;
 
-var grosspay = calculation.multiplication(rate, hours, days);
+var grosspay = calculation.multiplication(rate, hours);
+var grosspay2 = calculation.multiplication(grosspay, days);
 var tax = calculation.multiplication(grosspay, taxrate);
-var deduction = calculation.add(tax, sss, pagibig, philhealth);
-var netsal = calculation.subtract(grosspay, deduction);
+var deduction = calculation.add(tax, sss);
+var deduction2 = calculation.add(pagibig, philhealth);
+var deduction3 = calculation.add(deduction, deduction2);
+var netsal = calculation.subtract(grosspay2, deduction3);
 
-console.log('The gross income is: ' + grosspay);
+console.log('The gross income is: ' + grosspay2);
 console.log('Tax: ' + tax);
 console.log('SSS: ' + sss);
 console.log('Pag-ibig fund: ' + pagibig);
 console.log('PhilHealth: ' + philhealth);
-console.log('Total deduction: ' + deduction);
+console.log('Total deduction: ' + deduction3);
 console.log('The Net Salary is: ' + netsal);

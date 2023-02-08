f9cf09908e83a48f6f10358b7c9ce3b871291be6
xxxtwitterxxx06 <123307758+xxxtwitterxxx06@users.noreply.github.com>
2023-02-08 16:09:32 +0800

    1 file modified

--- prelims/lab-exam/greetings.js
+++ prelims/lab-exam/greetings.js
@@ -1,13 +1,13 @@
 function greet(name) {
-  return 'Hello' + name;
+  return 'Hello, ' + name;
 }
 
 function congratulations(name) {
-  return 'Congratulations' + name;
+  return 'Congratulations, ' + name;
 }
 
 function farewell(name) {
-  return 'farewell' + name;
+  return 'farewell, ' + name;
 }
 
 module.exports = { greet, congratulations, farewell };

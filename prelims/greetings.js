a8a881f6ae913a1b5c6c60a1767fcee7a8ba42ee
xxxtwitterxxx06 <123307758+xxxtwitterxxx06@users.noreply.github.com>
2023-02-08 16:00:39 +0800

    2 files added

---
+++ prelims/greetings.js
@@ -0,0 +1,16 @@
+function greet (name);{
+  return "Hello!" + name
+
+}
+
+function congrats (name);{
+  return "Congrats!" + name
+
+}
+
+function farewell (name);{
+  return "Farewell" + name
+
+}
+
+module.exports {greet, congratulations, farewell};
---
+++ prelims/lab-exam/app.js
@@ -0,0 +1,8 @@
+var greetings = require('./greetings.js');
+
+
+console.log("Hello" + greet);
+console.log("congrats" + congrats);
+console.log("farewell" + farewell);
+
+

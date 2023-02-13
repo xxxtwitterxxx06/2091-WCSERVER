eb1fe5ddc7af0307fcc381f4064344c99412b173
xxxtwitterxxx06 <123307758+xxxtwitterxxx06@users.noreply.github.com>
2023-02-13 11:13:15 +0800

    1 file added

---
+++ prelims/lab-exam/message.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return 'Hello, ' + name;
+}
+
+function congratulations(name) {
+  return 'Congratulations, ' + name;
+}
+
+function farewell(name) {
+  return 'farewell, ' + name;
+}
+
+module.exports = { greet, congratulations, farewell };

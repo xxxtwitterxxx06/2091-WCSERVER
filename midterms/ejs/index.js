1d8a099a2a44e7a170eec4a213c093f177ce1cbd
xxxtwitterxxx06 <123307758+xxxtwitterxxx06@users.noreply.github.com>
2023-03-13 12:47:05 +0800

    3 files modified, 2 added

--- midterms/ejs/index.js
+++ midterms/ejs/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'My EJS App' });
